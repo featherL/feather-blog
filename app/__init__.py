9811982416e7671c9741bf927a09369b28d8024e
featherl <1343145150@qq.com>
2018-10-27 12:53:04 +0800

    update index

--- app/__init__.py
+++ app/__init__.py
@@ -16,8 +16,6 @@ def create_app(config_name):
     db.init_app(app)
 
     from .main import main as main_blueprint
-    from .api import api as api_blueprint
     app.register_blueprint(main_blueprint, url_prefix='/')
-    app.register_blueprint(api_blueprint, url_prefix='/api')
 
     return app
